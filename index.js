0b3fc635503623e96ac8d37ec7a897565b605932
Miranda Cascione <miranda.cascione@gmail.com>
2023-02-22 16:07:22 -0800

    completed last stetch task

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ function getLongReviews(array) {
     return longReviews;
   }
 
-  console.log(getLongReviews(reviews));
+  // console.log(getLongReviews(reviews));
   
 
 /* 💪💪💪💪💪💪💪💪💪💪 STRETCH 3: 💪💪💪💪💪💪💪💪💪💪 
@@ -238,11 +238,19 @@ Use the carMaker function below to do the following:
   It would return 110 because it was created with 10 as the odometer and we added 100 to it with the drive method 
 */
 
+function carMaker(odometer) {
+  const car = {};
+  car.odometer = odometer;
 
-function carMaker(/* code here */) {
-    /* code here */
-    
+  car.drive = function (distance){
+    this.odometer += distance; 
+    return this.odometer;
+  } 
+  return car;
 }
+
+const car1 = carMaker(10);
+console.log('Expected 110. Got: ', car1.drive(100));
 
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
